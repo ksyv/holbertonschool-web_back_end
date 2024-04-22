dbe6e11c211cd01926f70f1aa6de3c4ed1d89e8b
ksyv <viais.kevin@hotmail.fr>
2024-04-22 11:51:16 +0200

    add 0-add.py for task 0 "add two float"

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Modul for task 0:
+Write a type-annotated function add that takes 
+a float a and a float b as arguments 
+and returns their sum as a float.
+"""
+
+def add(a: float, b: float) -> float:
+    """add two float argument
+
+    Args:
+        a (float): first float argument
+        b (float): second float argument
+
+    Returns:
+        float: sum of a and b
+    """
+    return a + b
