7b165576c7401e4c5cc4bcb0b0aff95e646f55d8
ksyv <viais.kevin@hotmail.fr>
2024-04-22 12:02:30 +0200

    add 1-concat.py for task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Modul for task 1:
+Write a type-annotated function concat that takes
+a string str1 and a string str2 as arguments
+and returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenated two string argument
+
+    Args:
+        str1 (str): first string argument
+        str2 (str): second string argument
+
+    Returns:
+        str: concatenated string of str1 and str2
+    """
+    return str1 + str2
