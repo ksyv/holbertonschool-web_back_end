484513f66b1575b4f55e8f845038ab65a5284cc8
ksyv <viais.kevin@hotmail.fr>
2024-04-22 11:55:55 +0200

    fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
 Modul for task 0:
-Write a type-annotated function add that takes 
-a float a and a float b as arguments 
+Write a type-annotated function add that takes
+a float a and a float b as arguments
 and returns their sum as a float.
 """
+
 
 def add(a: float, b: float) -> float:
     """add two float argument
