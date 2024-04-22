44ff4760fd73297f26e02d78e3919bf5194ae8a6
ksyv <viais.kevin@hotmail.fr>
2024-04-22 15:39:21 +0200

    add 3-to_str.py for task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Modul for task 2:
+Write a type-annotated function to_str that takes
+a float n as arguments
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """return the string representation of a float
+
+    Args:
+        n (float): float argument
+
+    Returns:
+        str: string representation of argument
+    """
+    return str(n)
