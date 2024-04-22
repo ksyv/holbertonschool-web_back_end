c93c8b6ffc0e9a98f0ed7b7d9a045e95e71504a9
ksyv <viais.kevin@hotmail.fr>
2024-04-22 12:11:50 +0200

    add 2-floor.py for task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Modul for task 2:
+Write a type-annotated function floor which takes
+a float n as arguments
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """return thefloor of a float
+
+    Args:
+        n (float): float argument
+
+    Returns:
+        int: floor of argument
+    """
+    return int(n)
