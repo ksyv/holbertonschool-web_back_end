f15071c29d15df2115f32c93ea790fbfa57999cd
ksyv <viais.kevin@hotmail.fr>
2024-04-24 20:38:10 +0200

    try change descrition

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,12 +3,6 @@
 Modul for task 10:
 Augment the following code
 with the correct duck-typed annotations:
-# The types of the elements of the input are not known
-def safe_first_element(lst):
-    if lst:
-        return lst[0]
-    else:
-        return None
 """
 from typing import Any, Sequence, Optional
 
