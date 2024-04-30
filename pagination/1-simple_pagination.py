326a6d316174555d5cbaefd5fcf6921918f99152
ksyv <viais.kevin@hotmail.fr>
2024-04-30 10:42:27 +0200

    add description for the last def

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -45,6 +45,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """adapt start_index and end_index at the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         dataset = self.dataset()
