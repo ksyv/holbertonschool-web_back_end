73fc846305f19951430977e79d0490e7f407333f
ksyv <viais.kevin@hotmail.fr>
2025-03-06 16:00:58 +0100

    add class redactingFormatter with List in fields and imlementation of message for formaat method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Log filter module
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -22,3 +23,22 @@ def filter_datum(fields: List[str], redaction: str,
     """
     return re.sub(r"("+separator+")(" + "|".join(fields)
                   + r")=([^"+separator+"]*)", r"\1\2="+redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecord to a string, redacting sensitive fields"""
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message,
+                            self.SEPARATOR)
