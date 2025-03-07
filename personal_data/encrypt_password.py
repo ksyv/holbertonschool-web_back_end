8db99d629c61279e16fcfb11bcd60f1f2df04d85
ksyv <viais.kevin@hotmail.fr>
2025-03-07 17:07:39 +0100

    Add file for encrypt password with bcrypt and check with used the same hached method

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,37 @@
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+        password: The password to hash.
+
+    Returns:
+        The salted, hashed password as a byte string.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a password matches a hashed password using bcrypt.
+
+    Args:
+        hashed_password: The hashed password as a byte string.
+        password: The password to check.
+
+    Returns:
+        True if the password matches the hashed password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+if __name__ == '__main__':
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
