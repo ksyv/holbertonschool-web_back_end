765f566fd404459714e0f1e4471157a1ac39107a
ksyv <viais.kevin@hotmail.fr>
2025-03-10 22:01:08 +0100

    fix pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(401)
 def unauthorised(error) -> str:
     """unauthorised handler
