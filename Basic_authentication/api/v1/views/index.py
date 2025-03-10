6c25165f5e3faa14bc45e6586a2b79127166c132
ksyv <viais.kevin@hotmail.fr>
2025-03-10 22:08:46 +0100

    implement handler for error 403 if it's a forbidden way

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - rises an error 401
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+    - rises an error 403
+    """
+    abort(403)
