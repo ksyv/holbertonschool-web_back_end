266d731d56ec5eb702d76523accc79476b0d54ff
ksyv <viais.kevin@hotmail.fr>
2025-03-12 09:16:53 +0100

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth that returns True if the path is not in the list of strings excluded_paths

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth:
         """
         Return: False
         """
-        return (False)
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return (True)
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return (False)
+        return (True)
 
     def authorization_header(self, request=None) -> str:
         """
