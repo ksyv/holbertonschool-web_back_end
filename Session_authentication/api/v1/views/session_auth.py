ad652c1bb6daf73c13a039a4c5cb616b361cc99f
ksyv <viais.kevin@hotmail.fr>
2025-03-12 15:28:28 +0100

    Update the class SessionAuth by adding a new method def destroy_session(self, request=None): that deletes the user session / logout:

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -33,3 +33,12 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Handles logout for session authentication."""
+    from api.v1.app import auth  # Import here to avoid circular import
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
