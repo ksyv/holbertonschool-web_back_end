a429fef16d8d238ba38508fe4b78b7790257fe57
ksyv <viais.kevin@hotmail.fr>
2025-03-12 15:29:52 +0100

    fix pycodestyle

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -34,6 +34,7 @@ def login():
 
     return response
 
+
 @app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Handles logout for session authentication."""
