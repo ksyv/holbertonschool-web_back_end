05ab9f26094a47cd159af0703a3caea249e5a00f
ksyv <viais.kevin@hotmail.fr>
2025-03-12 10:34:06 +0100

    Add the method def extract_base64_authorization_header(self, authorization_header: str) -> str: in the class BasicAuth that returns the Base64 part of the Authorization header for a Basic Authentication

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class who extends the Auth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
+            return (None)
+        if 'Basic' not in authorization_header:
+            return (None)
+        return authorization_header[6:]
