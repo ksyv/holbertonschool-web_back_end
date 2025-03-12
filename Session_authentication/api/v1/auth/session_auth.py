dbcc936b34174fb805b4595c548ed3794a684679
ksyv <viais.kevin@hotmail.fr>
2025-03-12 14:59:44 +0100

    Create a class attribute user_id_by_session_id initialized by an empty dictionary Create an instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id: Return None if user_id is None Return None if user_id is not a string Otherwise: Generate a Session ID using uuid module and uuid4() like id in Base Use this Session ID as key of the dictionary user_id_by_session_id - the value for this key must be user_id Return the Session ID The same user_id can have multiple Session ID - indeed, the user_id is the value in the dictionary user_id_by_session_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 """modul for manage session authentication
 """
-from flask import request
-from typing import List, TypeVar
-from models.user import User
 from api.v1.auth.auth import Auth
-import re
+import uuid
 
 
 class SessionAuth(Auth):
     """class for manage session authentication
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id."""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
