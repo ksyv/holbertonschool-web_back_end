13199f76462c602c0e03c35b4a0413c647fdc3be
ksyv <viais.kevin@hotmail.fr>
2025-03-25 16:17:34 +0100

    add 3-app.py with no change

---
+++ i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Modul for initialise flask with babel for en and fr"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+def get_locale():
+    """to determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=get_locale)
+
+
+class Config:
+    """"to config different languages"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def renderIndex():
+    """This is the function index"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
