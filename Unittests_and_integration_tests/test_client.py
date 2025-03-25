c29ab1bdca6791a2a3dacd532a19a1d13158c7d1
ksyv <viais.kevin@hotmail.fr>
2025-03-25 11:16:45 +0100

    add test_has_license method

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -58,6 +58,17 @@ class TestGithubOrgClient(unittest.TestCase):
             get_json_mock.assert_called_once_with("www.kevin.com")
             y.assert_called_once_with()
 
+    @parameterized.expand([
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license, expected):
+        """
+        Method tests the license checker
+        """
+        self.assertEqual(GithubOrgClient.has_license(
+            repo, license), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
