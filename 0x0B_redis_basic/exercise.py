19c6e3ed65a982ed510f6d29a172bd0ddfaf17d1
ksyv <viais.kevin@hotmail.fr>
2025-04-09 12:03:16 +0200

    Add def get, get_str et get_int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Modul for writing string to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -21,3 +21,22 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+    
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        take a key string argument and an optional Callable argument 
+        named fn. This callable will be used to convert the data back to the 
+        desired format.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> Union[str, None]:
+        """Get a str by Redis."""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Get an int by Redis."""
+        return self.get(key, fn=int)
