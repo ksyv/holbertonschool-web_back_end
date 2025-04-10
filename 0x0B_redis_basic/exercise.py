9614c664853fb0129b963d87fa7d1003505092b4
ksyv <viais.kevin@hotmail.fr>
2025-04-10 13:13:47 +0200

    fix pycode

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -14,12 +14,13 @@ def count_calls(method: Callable) -> Callable:
 
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapper around the method to increment the counter and call the original method."""
+        """Wrapper around the method to increment the counter."""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
 
-def call_history(method:Callable) -> Callable:
+
+def call_history(method: Callable) -> Callable:
     """decorator to store the history of inputs ans outputs"""
     inputs_key = f"{method.__qualname__}:inputs"
     outputs_key = f"{method.__qualname__}:outputs"
@@ -32,6 +33,7 @@ def call_history(method:Callable) -> Callable:
         self._redis.rpush(outputs_key, output)
         return output
     return wrapper
+
 
 def replay(method: Callable):
     """Displays the history of calls of a particular function."""
@@ -67,10 +69,11 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int,
+                                                          float, None]:
         """
-        take a key string argument and an optional Callable argument 
-        named fn. This callable will be used to convert the data back to the 
+        take a key string argument and an optional Callable argument
+        named fn. This callable will be used to convert the data back to the
         desired format.
         """
         data = self._redis.get(key)
