afb4e724a787fc8188cf044c605d0220626d30c3
ksyv <ksyviais@gmail.com>
2025-05-22 12:30:10 +0200

    add files for task 10 & 11

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,48 @@
+import kue from 'kue';
+import { expect } from 'chai'; 
+import createPushNotificationsJobs from './8-job.js';
+
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  beforeEach(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter(); 
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+    queue.testMode.exit();
+  });
+
+  it('display a error message if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs('not an array', queue)).to.throw('Jobs is not an array');
+    expect(() => createPushNotificationsJobs({}, queue)).to.throw('Jobs is not an array');
+    expect(() => createPushNotificationsJobs(null, queue)).to.throw('Jobs is not an array');
+    expect(() => createPushNotificationsJobs(123, queue)).to.throw('Jobs is not an array');
+  });
+
+  it('create new jobs to the queue', () => {
+    const list = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+      }
+    ];
+
+    createPushNotificationsJobs(list, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code');
+    expect(queue.testMode.jobs[0].data).to.deep.equal(list[0]);
+
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code');
+    expect(queue.testMode.jobs[1].data).to.deep.equal(list[1]);
+  });
+});
